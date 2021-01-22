19fe022cf8d1de53126b836599e7dde2edaac2b6
praczkowski <p.raczkowski@ocado.com>
2021-01-23 00:16:26 +0100

    Adding advanced examples 8

--- README.md
+++ README.md
@@ -50,6 +50,7 @@ App class:
 * Make the main thread to await on CountDownLatch for all threads to stop waiting.
 
 ## Exercise 8:
-* Make one thread adding M to name
-* Make another thread A to name
-* Try to write MAMA using Exchanger class
+* Create class ThreadA
+* Create class ThreadB
+* Using Exchanger, exchange from ThreadA string "from A" and print it
+* Using Exchanger, exchange from ThreadA string "from B" and print it
--- src/main/java/com/codecool/threads/advanced/excercises/excercise8/Main.java
+++ src/main/java/com/codecool/threads/advanced/excercises/excercise8/Main.java
@@ -1,22 +1,8 @@
 package com.codecool.threads.advanced.excercises.excercise8;
 
-import java.util.concurrent.Exchanger;
-
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Exchanger<Word> exchanger = new Exchanger<>();
-        Word word = new Word("");
+    public static void main(String[] args) {
 
-        MConcatenator mConcatenator = new MConcatenator(exchanger, word);
-        AConcatenator aConcatenator = new AConcatenator(exchanger, word);
-
-        mConcatenator.start();
-        aConcatenator.start();
-
-        mConcatenator.join();
-        aConcatenator.join();
-
-        System.out.println(word.getName());
     }
 }
